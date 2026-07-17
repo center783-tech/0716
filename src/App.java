13a4870eca57cb68803bf2547df78e2795f927f1
center783-tech <center783@gmail.com>
2026-07-17 22:07:12 +0800

    dev cherry pick amin
    (cherry picked from commit e4f52595d72f61ae095c2cff1a18d2baecdf65d1)

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("main #1");
+
+        System.out.println("dev #1");
+
         System.out.println("admin #1");
         System.out.println("main #2");
         System.out.println("admin #2");
-    }
+        System.out.println("dev #2");
+
+}
 }
