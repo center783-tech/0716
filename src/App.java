fb5a75751c70923cdc49d32bdb523753ee658c44
center783-tech <center783@gmail.com>
2026-07-17 22:26:27 +0800

    updata 修改admin App.java#3

--- src/App.java
+++ src/App.java
@@ -4,5 +4,7 @@ public class App {
         System.out.println("admin #1");
         System.out.println("main #2");
         System.out.println("admin #2");
+        System.out.println("admin #3");
+
     }
 }
